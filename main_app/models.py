f3182396d7d09b97d7afc1c1b5dae2ebff4aa0eb
helenamarkus14 <helenamarkus14@gmail.com>
2022-04-02 15:09:53 -0600

    created Cookie model, view, url, template and login

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
+
 # Create your models here.
+
+RATING_CHOICES = [
+    ('1','1'),
+    ('2','2'),
+    ('3','3'),
+    ('4','4'),
+    ('5','5'),
+    ('6','6'),
+    ('7','7'),
+    ('8','8'),
+    ('9','9'),
+    ('10','10'),
+    ]
+
+# Cookie
+class Cookie(models.Model):
+    name = models.CharField(max_length=50)
+    img = models.ImageField(null=True, blank=True, upload_to="images/")
+    description = models.CharField(max_length=250),
+    appearance = models.CharField(max_length=3, choices = RATING_CHOICES, default='1')
+    taste =  models.CharField(max_length=3, choices = RATING_CHOICES, default='1')
+    overall_rating = models.CharField(max_length=3, choices = RATING_CHOICES, default='1')
+    comments = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.views import View # <- View class to handle requests
 from django.http import HttpResponse, HttpResponseRedirect # <- a class to handle sending a type of response
-# from .models import 
+from .models import Cookie
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import DetailView
 from django.urls import reverse
@@ -18,3 +18,50 @@ from django.utils.decorators import method_decorator
 # Create your views here.
 class Home(TemplateView):
     template_name = 'home.html'
+
+
+# Cookie Views
+class Cookies(TemplateView):
+    template_name='cookies.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        name = self.request.GET.get('name')
+
+        if name !=None:
+            context["cookies"] = Cookie.objects.filter(name__icontains=name)
+            context['header'] = f'searching for {name}'
+        else:
+            context['cookies'] = Cookie.objects.all()
+            context['header'] = "Cookies"
+        return context        
+
+
+
+
+
+
+
+# Bakery Views
+# Login, Logout, and SignUp
+
+def login_view(request):
+    # if post, then authenticate (user submitted username and password)
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password'] 
+            user = authenticate(username = u, password = p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/user/'+u)
+                else:
+                    return render(request, 'login.html', {'form': form})
+                    # feel free to redirect them somewhere
+            else: 
+                return render(request, 'signup.html', {'form': form})
+    else:
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})    
